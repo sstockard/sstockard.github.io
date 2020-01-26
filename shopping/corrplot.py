6a15ecfed309a50f46bf1dcf94004b7bd0e0c4aa
sstockard <42778469+sstockard@users.noreply.github.com>
2020-01-26 00:17:17 -0500

    Create corrplot.py

---
+++ shopping/corrplot.py
@@ -0,0 +1,14 @@
+features=pd.read_csv("features-data-set.csv") 
+
+some = features[['Temperature','Fuel_Price','CPI','Unemployment']]
+
+corr = features.corr()
+
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+f, ax = plt.subplots(figsize=(11, 9))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
